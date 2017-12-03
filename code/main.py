6288d209f2a5bec29eaf8498e5c5eb73ed1a2ddb
Jayant <jayantjnp@gmail.com>
2017-12-03 05:04:22 -0500

    scaled

--- code/main.py
+++ code/main.py
@@ -16,9 +16,24 @@ filename = '../data/CelebA/Anno/list_attr_celeba.txt'
 imagepath = '../data/CelebA/Img/img_align_celeba/'
 celebData = 0
 (celebData, labels, imageNames) = dataloader(imagepath, filename)
+celebData = celebData[0:100000,:,:,:]
+labels = labels[0:100000,:]
+imageNames = imageNames[0:100000]
 print("Celebdata dimension is ", celebData.shape)
 print("labels dimension is ", labels.shape)
 print("imageNames dimension is ", imageNames.shape)
+
+# view_image(normalized_X[9999,:,:], train_images_label[9999])
+trains_images = celebData[0:20000,:,:,:]#getting the training sets
+test_images = celebData[20000:25000,:,:,:]#getting the training sets
+train_images_labels = labels[0:20000,:]
+test_images_labels = labels[20000:25000,:]
+trains_images = trains_images.reshape([20000,2475*3])#flattening the input array
+test_images = test_images.reshape([5000,2475*3])
+
+
+trains_images = preprocessing.scale(trains_images)#standardising the image data set with zero mean and unit standard deviation
+test_images = preprocessing.scale(test_images)
 
 #eyglasses at column 15+1
 #Extract data
